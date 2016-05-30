73dd7cc86659008c652a6dbb20a3c2af26f6c609
jpann <ikonmobile@gmail.com>
2016-05-29 18:39:22 -0600

    Test fetching channel and video metadata for video listing

--- plugin.video.full30/addon.py
+++ plugin.video.full30/addon.py
@@ -12,111 +12,142 @@ xbmcplugin.setContent(_handle, 'tvshows')
 
 def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
-    
+
 url_base = "https://www.full30.com"
 full30 = Full30(url_base)
 
 def get_channels():
 	listing = []
-	
+
+
 	channels = full30.get_channels()
-	
+
 	for channel in channels:
 		list_item = xbmcgui.ListItem(channel['name'],  iconImage=channel['thumbnail'])
-		
+
 		list_item.setInfo('video', {'title': channel['name']})
-		
+
 		url = '{0}?action=list-video-type&channel_url={1}'.format(_url, channel['url'])
-		
+
 		is_folder = True
-		
+
 		listing.append((url, list_item, is_folder))
-		
+
 	xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
-	
+
 	xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-	
+
 	xbmcplugin.endOfDirectory(_handle)
 
 def list_video_type(channel_url):
 	list_item = xbmcgui.ListItem(label='Featured',  iconImage='DefaultVideo.png')
 	url = '{0}?action=listing&type=featured&channel_url={1}'.format(_url, channel_url)
 	xbmcplugin.addDirectoryItem(handle=_handle, url=url, listitem=list_item, isFolder=True)
-	
+
 	list_item = xbmcgui.ListItem(label='Recent',  iconImage='DefaultVideo.png')
 	url = '{0}?action=listing&type=recent&channel_url={1}'.format(_url, channel_url)
 	xbmcplugin.addDirectoryItem(handle=_handle, url=url, listitem=list_item, isFolder=True)
-	
+
 	xbmcplugin.endOfDirectory(_handle)
 
 def list_videos_featured(channel_url):
 	featured_videos = full30.get_featured(channel_url)
-	
+
 	listing = []
-	
-	for video in featured_videos:
+
+	for video in featured_videos['videos']:
+		# Fetch video's metadata from the video's page
+		meta_data = full30.get_video_metadata(video['hash'])
+
 		video_url = full30.get_video_mp4(video['url'])
-		
+
 		list_item = xbmcgui.ListItem(label=video['title'], iconImage=video['thumbnail'])
-		
-		list_item.setInfo('video', {'title': video['title'] })
-		 
+
+		list_item.setInfo('video',
+			{
+				'title': video['title'],
+				'year' : meta_data['uploaded'].year,
+				'plot' : meta_data['desc']
+			})
+
+		list_item.setArt(
+			{
+				'thumb' : video['thumbnail'],
+				'poster' : video['thumbnail'],
+				'fanart' : featured_videos['metadata']['image']
+			})
+
 		list_item.setProperty('IsPlayable', 'true')
-		 
+
 		url = '{0}?action=play&video={1}'.format(_url, video_url)
-		 
+
 		is_folder = False
-		 
+
 		listing.append((url, list_item, is_folder))
-		 
+
 	xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
-	
+
 	xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
 
 	xbmcplugin.endOfDirectory(_handle)
 
-def list_videos_recent_pages(channel_url, page = 1):		
-	featured_videos = full30.get_recent_by_page(channel_url, page)
-	
-	listing = []
-	
-	pages = featured_videos['pages']
+def list_videos_recent_pages(channel_url, page = 1):
+	recent_videos = full30.get_recent_by_page(channel_url, page)
 
-	for video in featured_videos['videos']:
+	listing = []
+
+	pages = recent_videos['pages']
+
+	for video in recent_videos['videos']:
+		# Fetch video's metadata from the video's page
+		meta_data = full30.get_video_metadata(video['hash'])
+
 		video_url = video['mp4_url']
-		
+
 		list_item = xbmcgui.ListItem(label=video['title'], iconImage=video['thumbnail'])
-		
-		list_item.setInfo('video', {'title': video['title'] })
-		 
+
+		list_item.setInfo('video',
+			{
+				'title': video['title'],
+				'year' : meta_data['uploaded'].year,
+				'plot' : meta_data['desc']
+			})
+
+		list_item.setArt(
+			{
+				'thumb' : video['thumbnail'],
+				'poster' : video['thumbnail'],
+				'fanart' : recent_videos['metadata']['image']
+			})
+
 		list_item.setProperty('IsPlayable', 'true')
-		 
+
 		url = '{0}?action=play&video={1}'.format(_url, video_url)
-		 
+
 		is_folder = False
-		 
+
 		listing.append((url, list_item, is_folder))
-		 
+
 	xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
-	
+
 	next_page = int(page) + 1
 	if next_page <= pages:
 		list_item = xbmcgui.ListItem(label='Page ' + str(next_page) + ' >>',  iconImage='DefaultVideo.png')
-		
+
 		url = '{0}?action=listing&type=recent&channel_url={1}&page={2}'.format(_url, channel_url, next_page)
-		
+
 		xbmcplugin.addDirectoryItem(handle=_handle, url=url, listitem=list_item, isFolder=True)
 
 	xbmcplugin.endOfDirectory(_handle)
-	
+
 def play_video(path):
 	play_item = xbmcgui.ListItem(path=path)
-	
+
 	xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
-	
+
 def router(paramstring):
 	params = dict(parse_qsl(paramstring))
-	
+
 	if params:
 		if params['action'] == 'listing':
 			if params['type'] == 'featured':
@@ -132,6 +163,6 @@ def router(paramstring):
 			play_video(params['video'])
 	else:
 		get_channels()
-		
+
 if __name__ == '__main__':
     router(sys.argv[2][1:])
--- plugin.video.full30/full30.py
+++ plugin.video.full30/full30.py
@@ -1,19 +1,22 @@
 import urllib2
 from bs4 import BeautifulSoup
+from datetime import datetime
+import time
 import requests
 
 class Full30:
     def __init__(self, url):
         self.opener = urllib2.build_opener()
         self.opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        
+
         self.url = url
         self.channels_url = self.url + "/channels/all"
         self.video_url = self.url + "/video/{0}"
         self.api_recent_url = self.url + "/api/v1.0/channel/{0}/recent-videos?page={1}"
         self.thumbnail_url = self.url + "/cdn/videos/{0}/{1}/thumbnails/320x180_{2}.jpg"
-	    self.mp4_url = "https://videos.full30.com/bitmotive/public/full30/v1.0/videos/{0}/{1}/640x360.mp4"
-       
+        self.mp4_url = "https://videos.full30.com/bitmotive/public/full30/v1.0/videos/{0}/{1}/640x360.mp4"
+        self.channel_url = self.url + "/channels/{0}"
+
     def get_channels(self):
         channels = []
 
@@ -25,57 +28,86 @@ class Full30:
         videostreams = soup.find(class_="video-stream")
 
         for video in videostreams.find_all('div'):
+            channel_slug = url.rsplit('/', 1)[-1]
+
+            channel_metadata = self.get_channel_metadata(channel_slug)
+
             channel_url = self.url + video.find('a').get('href')
             channel_name = video.find('h4', class_='thumbnail-title').string.encode('ascii', 'ignore').decode('ascii')
             channel_thumbnail = "http:" + video.find('img', class_="thumbnail").get('src')
+            channel_image = channel_metadata['image']
+            channel_desc = channel_metadata['desc']
 
-            channels.append({ "name" : channel_name, "url" : channel_url, "thumbnail" : channel_thumbnail })
+            channels.append(
+                {
+                    "name" : channel_name,
+                    "url" : channel_url,
+                    "thumbnail" : channel_thumbnail,
+                    'image' : channel_image,
+                    'desc' : channel_desc
+                })
 
         return channels
-        
+
     def get_featured(self, url):
-        featured = []
+        featured = { 'title' : '', 'slug' : '', 'pages' : '', 'videos' : [] }
+
+        channel_slug = url.rsplit('/', 1)[-1]
 
         html = self.opener.open(url)
         data = html.read()
 
         soup = BeautifulSoup(data, "html.parser")
 
+        metadata = self.get_channel_metadata_from_data(soup)
+
+        featured['title'] = soup.find('meta', property='og:title').get('content')
+        featured['slug'] = channel_slug
+        featured['metadata'] = metadata
+
         featured_videos = soup.find(class_="thumbnail-wrapper featured-thumbnail-wrapper")
-        
+
         if featured_videos:
             for video in featured_videos.find_all('div', class_="thumbnail-box"):
                 video_url = self.url + video.find('a').get('href').encode('ascii', 'ignore').decode('ascii')
                 video_name = video.find('h4', class_='thumbnail-title').string.encode('ascii', 'ignore').decode('ascii')
                 video_thumbnail = "http:" + video.find('img', class_="thumbnail").get('src')
+                video_hash = video_url.rsplit('/', 1)[-1]
 
-                featured.append({ "title" : video_name, "url" : video_url, "thumbnail" : video_thumbnail })
+                featured['videos'].append(
+                    {
+                        "title" : video_name,
+                        "url" : video_url,
+                        "thumbnail" : video_thumbnail,
+                        "hash" : video_hash
+                    })
 
         return featured
-    
+
     def get_recent_by_page(self, url, page):
         recent = { 'title' : '', 'slug' : '', 'pages' : '', 'videos' : [] }
-        
-	if not page:
-		page = 1
-	
-	channel_name = url.rsplit('/', 1)[-1]
-	api_url = self.api_recent_url.format(channel_name, page)
-        
+
+        if not page:
+            page = 1
+
+        channel_name = url.rsplit('/', 1)[-1]
+        api_url = self.api_recent_url.format(channel_name, page)
+
         r = requests.get(api_url, headers={'User-Agent' : 'Mozilla/5.0'})
-        
+
         if not r:
             return None
-            
+
         data = r.json()
-        
+
         if not data:
             return None
-            
+
         recent['title'] = data['channel']['title']
         recent['pages'] = data['pages']
         recent['slug'] = data['channel']['slug']
-        
+        recent['metadata'] = self.get_channel_metadata(data['channel']['slug'])
+
         for video in data['videos']:
             v_hash = video['hashed_identifier']
             v_id = video['id']
@@ -84,28 +116,8 @@ class Full30:
             v_url = self.video_url.format(v_hash)
             v_channel = recent['title']
             v_mp4_url = self.mp4_url.format(recent['slug'], v_hash)
-            
-            recent['videos'].append({ "title" : v_title, "url" : v_url, "thumbnail" : v_thumbnail, "channel": v_channel, "mp4_url" : v_mp4_url })
-        
-        return recent
-    
-    def get_recent(self, url):
-        recent = []
 
-        html = self.opener.open(url)
-        data = html.read()
-
-        soup = BeautifulSoup(data, "html.parser")
-
-        recent_videos = soup.find(class_="thumbnail-wrapper recent-thumbnail-wrapper")
-        
-        if recent_videos:
-            for video in recent_videos.find_all('div', class_="thumbnail-box"):
-                video_url = self.url + video.find('a').get('href').encode('ascii', 'ignore').decode('ascii')
-                video_name = video.find('h4', class_='thumbnail-title').string.encode('ascii', 'ignore').decode('ascii')
-                video_thumbnail = "http:" + video.find('img', class_="thumbnail").get('src')
-
-                recent.append({ "title" : video_name, "url" : video_url, "thumbnail" : video_thumbnail })
+            recent['videos'].append({ "title" : v_title, "url" : v_url, "thumbnail" : v_thumbnail, "channel": v_channel, "mp4_url" : v_mp4_url, "hash" : v_hash })
 
         return recent
 
@@ -121,8 +133,60 @@ class Full30:
 
         if video:
             video_source = video.find('source', type='video/mp4; codecs="avc1.64001E, mp4a.40.2"')
-            
+
             if video_source:
                 video_url = video_source.get('src')
-                
+
         return video_url
+
+    def get_video_metadata(self, video_hash):
+        metadata = {}
+
+        url = self.video_url.format(video_hash)
+
+        html = self.opener.open(url)
+        data = html.read()
+
+        soup = BeautifulSoup(data, "html.parser")
+
+        uploaded = soup.find('span', class_="fa fa-calendar").text.replace("Uploaded:", "").strip()
+        date = ''
+
+        if uploaded:
+            try:
+                date = datetime.strptime(uploaded, "%m/%d/%Y")
+            except TypeError:
+                date = datetime.fromtimestamp(time.mktime(time.strptime(uploaded, "%m/%d/%Y")))
+
+        metadata['desc'] = soup.find('meta', property='og:description').get('content')
+        metadata['uploaded'] = date
+        metadata['views'] = soup.find('span', id="player-view-count").text
+
+        return metadata
+
+    def get_channel_metadata(self, slug):
+        metadata = {}
+
+        url = self.channel_url.format(slug)
+
+        html = self.opener.open(url)
+        data = html.read()
+
+        soup = BeautifulSoup(data, "html.parser")
+
+        metadata['url'] = soup.find('meta', property='og:url').get('content')
+        metadata['title'] = soup.find('meta', property='og:title').get('content')
+        metadata['desc'] = soup.find('meta', property='og:description').get('content')
+        metadata['image'] = self.url + soup.find('img', class_='channel-banner').get('src')
+
+        return metadata
+
+    def get_channel_metadata_from_data(self, soup):
+        metadata = {}
+
+        metadata['url'] = soup.find('meta', property='og:url').get('content')
+        metadata['title'] = soup.find('meta', property='og:title').get('content')
+        metadata['desc'] = soup.find('meta', property='og:description').get('content')
+        metadata['image'] = self.url + soup.find('img', class_='channel-banner').get('src')
+
+        return metadata
